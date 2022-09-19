5e169df34e097dd8b8660163b89f984ef1d28998
Aditya Kumar <itsadityamishra15@gmail.com>
2022-09-19 16:31:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! this my app..Its working on AWS Amplify by Aditya 
+          Hello! this my app..Its working on AWS Amplify by Tanul
         </p>
         <a
           className="App-link"
